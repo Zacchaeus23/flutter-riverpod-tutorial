75a34f6ba0bb15641829290d6933bf88580e66e0
Michael Zacchaeus <108746426+Zacchaeus23@users.noreply.github.com>
2024-07-10 14:59:47 +0100

    Using Notifier provider

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,24 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:riverpod_files/models/product.dart';
+
+class CartNotifier extends Notifier<Set<Product>> {
+  @override
+  Set<Product> build() {
+
+    //initial value
+    return const {
+      Product(
+        id: '4',
+        title: 'Red Backpack',
+        price: 14,
+        image: 'assets/products/backpack.png',
+      ),
+    };
+  }
+
+  //methods to update state
+}
+
+final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
+  return CartNotifier();
+});
